e7691b8dffa82d9c9f139a4cf9cfb514baa95be2
Salman Farshe <salman.zihad@involve.ai>
2023-11-07 02:09:26 +0600

    Basic Explore

---
+++ Intro/index.ts
@@ -0,0 +1,9 @@
+let userName: string = "Salman Farshe"
+let age: number = 25
+console.log("Name is " + userName);
+console.log("Age is " + age);
+
+function addNumbers(value1: number, value2: number) {
+  return value1 + value2;
+}
+console.log(addNumbers("a","10"));
