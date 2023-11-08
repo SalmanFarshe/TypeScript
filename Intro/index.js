bdc0e044d9ba86a2d03622cb5f5ece50df20a158
Salman Farshe <salman.zihad@involve.ai>
2023-11-08 11:52:14 +0600

    Updated

--- Intro/index.js
+++ Intro/index.js
@@ -5,4 +5,4 @@ console.log("Age is " + age);
 function addNumbers(value1, value2) {
     return value1 + value2;
 }
-console.log(addNumbers("a", "10"));
+console.log(addNumbers("5", 5));
--- Intro/index.ts
+++ Intro/index.ts
@@ -3,7 +3,7 @@ let age: number = 25
 console.log("Name is " + userName);
 console.log("Age is " + age);
 
-function addNumbers(value1: number, value2: number) {
+function addNumbers(value1: string, value2: number) {
   return value1 + value2;
 }
-console.log(addNumbers("a","10"));
+console.log(addNumbers("5",5));
