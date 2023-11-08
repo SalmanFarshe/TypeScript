7f003cb2d1f621c527a94d4011760e56eece7110
Salman Farshe <salman.zihad@involve.ai>
2023-11-08 13:11:11 +0600

    File Structure Changed

--- Build_In_Types/index.js
+++
@@ -1,5 +0,0 @@
-var firstName = "Salman";
-var lastName = " Farshe";
-var userId = 222400010;
-var activeStatus = true;
-console.log("\n   Name: ".concat(firstName + lastName, " \n   Id: ").concat(userId, " \n   Active Status: ").concat(activeStatus ? "Active" : "Ofline", "\n   "));
--- Intro/index.js
+++
@@ -1,8 +0,0 @@
-var userName = "Salman Farshe";
-var age = 25;
-console.log("Name is " + userName);
-console.log("Age is " + age);
-function addNumbers(value1, value2) {
-    return value1 + value2;
-}
-console.log(addNumbers("5", 5));
