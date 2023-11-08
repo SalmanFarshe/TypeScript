2461ec3bf8b5d48e568d24e1061d24f3af1aa710
Salman Farshe <salman.zihad@involve.ai>
2023-11-08 12:33:40 +0600

    Number String Boolrean

---
+++ Build_In_Types/index.ts
@@ -0,0 +1,10 @@
+let firstName: string = "Salman";
+let lastName: string = " Farshe";
+let userId: number = 222400010;
+let activeStatus: boolean = true;
+console.log(`
+   Name: ${firstName + lastName} 
+   Id: ${userId} 
+   Active Status: ${activeStatus ? "Active" : "Ofline"}
+   `
+);
