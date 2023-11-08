02b25faaa3e3c0c5b93585b051096457adff3936
Salman Farshe <salman.zihad@involve.ai>
2023-11-08 13:07:36 +0600

    Introduction To typescript

---
+++ 01.Intro/index.ts
@@ -0,0 +1,9 @@
+let userName: string = "Salman Farshe"
+let age: number = 25
+console.log("Name is " + userName);
+console.log("Age is " + age);
+
+function addNumbers(value1: string, value2: number) {
+  return value1 + value2;
+}
+console.log(addNumbers("5",5));
