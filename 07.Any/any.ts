c889415b6438fc34d4c1d02072895734ba5219df
Salman Farshe <salman.zihad@involve.ai>
2023-11-09 17:04:14 +0600

    Any

---
+++ 07.Any/any.ts
@@ -0,0 +1,16 @@
+let anyType: any;
+anyType = 1;
+console.log(typeof anyType);
+anyType = true;
+console.log(typeof anyType);
+anyType = "Allah Ho Akbar";
+console.log(typeof anyType);
+anyType = null;
+console.log(typeof anyType);
+anyType = [1, 2, "Islam"];
+console.log(typeof anyType);
+anyType = {
+  "1": "Ya hayyu ya kayyum",
+  "2": "Ya Allah",
+};
+console.log(typeof anyType);
