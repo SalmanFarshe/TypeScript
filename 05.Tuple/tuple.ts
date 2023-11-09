c007c2c90c2be3dc46bd18ec9852a8096114be3b
Salman Farshe <salman.zihad@involve.ai>
2023-11-09 17:03:16 +0600

    Tuple

--- 05.Tuple/tuple.ts
+++ 05.Tuple/tuple.ts
@@ -2,7 +2,7 @@
 // Tuples only have strongly defined types for the initial values:
 
 let data: [number, boolean, string];
-data = [5, false, 'Coding God was here'];
+data = [5, false, 'Coding Goes here'];
 
 // We have no type safety in our tuple for indexes 3+
 data.push('New');
